bd53974c8e0114ec133b953ad185acda8ddd48be
MFathinHalim <halimfathin7@gmail.com>
2024-05-02 15:17:44 +0700

    AKHIRNYA BACKENDNYA BISA SYNC KE FRONTEND

--- index.js
+++ index.js
@@ -1,98 +1,122 @@
 const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
-const https = require("https")
+const https = require("https");
 const { JSDOM } = require("jsdom");
 
 const leaderboardData = {
   users: [],
   roles: [],
-  multipliers: []
+  multipliers: [],
 };
 
 function https_fetch(url, path, method, headers, body, get_headers) {
-  if (body) headers["Content-Length"] = body.length
+  if (body) headers["Content-Length"] = body.length;
   return new Promise((resolve, reject) => {
-      const req = https.request({
-          hostname: url,
-          path: path,
-          method: method,
-          headers: {
-              'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:125.0) Gecko/20100101 Firefox/125.0',
-              'DNT': '1',
-              'Sec-GPC': '1',
-              'Connection': 'keep-alive',
-              'Upgrade-Insecure-Requests': '1',
-              'Sec-Fetch-Dest': 'document',
-              'Sec-Fetch-Mode': 'navigate',
-              'Sec-Fetch-Site': 'none',
-              'Sec-Fetch-User': '?1',
-              'TE': 'trailers', 
-              ...headers
-          },
-      }, (res) => {
-          if (get_headers) resolve(res.headers)
-          let data = '';
-          res.on('data', (chunk) => data += chunk);
-          res.on('end', () => resolve(data));
-      });
-      if (body) req.write(body)
-      req.end();
+    const req = https.request(
+      {
+        hostname: url,
+        path: path,
+        method: method,
+        headers: {
+          "User-Agent":
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:125.0) Gecko/20100101 Firefox/125.0",
+          DNT: "1",
+          "Sec-GPC": "1",
+          Connection: "keep-alive",
+          "Upgrade-Insecure-Requests": "1",
+          "Sec-Fetch-Dest": "document",
+          "Sec-Fetch-Mode": "navigate",
+          "Sec-Fetch-Site": "none",
+          "Sec-Fetch-User": "?1",
+          TE: "trailers",
+          ...headers,
+        },
+      },
+      (res) => {
+        if (get_headers) resolve(res.headers);
+        let data = "";
+        res.on("data", (chunk) => (data += chunk));
+        res.on("end", () => resolve(data));
+      }
+    );
+    if (body) req.write(body);
+    req.end();
   });
 }
 
-
 const refreshLeaderboardData = async (page = 1) => {
-  const data = await https_fetch("beta.lurkr.gg", `/levels/sciencekingdom?page=${page}`, "GET")
-  const dom = new JSDOM(data)
+  const data = await https_fetch(
+    "beta.lurkr.gg",
+    `/levels/sciencekingdom?page=${page}`,
+    "GET"
+  );
+  const dom = new JSDOM(data);
 
-  let honoka = dom.window.document.body.querySelectorAll("script[type]")[8].innerHTML
-  honoka = honoka.replace("self.__next_f.push(", "").slice(0, -1)
-  honoka = JSON.parse(honoka)
-  honoka = honoka[1]
-  honoka = honoka.substring(2, honoka.length)
-  honoka = JSON.parse(honoka)
+  let honoka =
+    dom.window.document.body.querySelectorAll("script[type]")[8].innerHTML;
+  honoka = honoka.replace("self.__next_f.push(", "").slice(0, -1);
+  honoka = JSON.parse(honoka);
+  honoka = honoka[1];
+  honoka = honoka.substring(2, honoka.length);
+  honoka = JSON.parse(honoka);
 
   // data lb user
-  const chika = honoka[3].children[1][3].children[0][3].children[0][3].data
+  const chika = honoka[3].children[1][3].children[0][3].children[0][3].data;
 
-  if (page === 1) leaderboardData.users = chika
-  else leaderboardData.users.push(...chika)
+  if (page === 1) leaderboardData.users = chika;
+  else leaderboardData.users.push(...chika);
 
   // data lb role reward
-  const ayumu = honoka[3].children[1][3].children[1][3].children[0][3].children[1][3].children
+  const ayumu =
+    honoka[3].children[1][3].children[1][3].children[0][3].children[1][3]
+      .children;
 
-  if (page === 1) leaderboardData.roles = ayumu.map(setsuna => {
-    const yuu = {
-      level: setsuna[3].children[0][3].children,
-      color: setsuna[3].children[1][3].children[0][3].children[0][3].style.backgroundColor,
-      name: setsuna[3].children[1][3].children[0][3].children[1][3].children
-    }
-    return yuu
-  })
+  if (page === 1)
+    leaderboardData.roles = ayumu.map((setsuna) => {
+      const yuu = {
+        level: setsuna[3].children[0][3].children,
+        color:
+          setsuna[3].children[1][3].children[0][3].children[0][3].style
+            .backgroundColor,
+        name: setsuna[3].children[1][3].children[0][3].children[1][3].children,
+      };
+      return yuu;
+    });
 
   // data lb multiplier
-  const kanon = honoka[3].children[1][3].children[1][3].children[1][3].children[1][3].children
+  const kanon =
+    honoka[3].children[1][3].children[1][3].children[1][3].children[1][3]
+      .children;
 
-  if (page === 1) leaderboardData.multipliers = kanon.map(shiki => {
-    const tomari = {}
-    tomari.multiplier = shiki[3].children[0][3].children
-    const wien = shiki[3].children[1][3].children
-    tomari.data = wien.map(chisato => {
-      const sumire = {}
-      sumire.name = chisato[3].children[1][3].children
-      const keke = chisato[3].children[0][3]
-      if (keke.style) {
-        sumire.type = "role"
-        sumire.color = keke.style.backgroundColor
-      } else if (keke.src === "$17" || keke.src.src === "/_next/static/media/text.41c9f660.svg") sumire.type = "textChannel"
-        else if (keke.src === "$18" || keke.src.src === "/_next/static/media/forum.192798a1.svg") sumire.type = "forumChannel"
-      return sumire
-    })
-    return tomari
-  })
+  if (page === 1)
+    leaderboardData.multipliers = kanon.map((shiki) => {
+      const tomari = {};
+      tomari.multiplier = shiki[3].children[0][3].children;
+      const wien = shiki[3].children[1][3].children;
+      tomari.data = wien.map((chisato) => {
+        const sumire = {};
+        sumire.name = chisato[3].children[1][3].children;
+        const keke = chisato[3].children[0][3];
+        if (keke.style) {
+          sumire.type = "Role";
+          sumire.color = keke.style.backgroundColor;
+        } else if (
+          keke.src === "$17" ||
+          keke.src.src === "/_next/static/media/text.41c9f660.svg"
+        )
+          sumire.type = "Channel";
+        else if (
+          keke.src === "$18" ||
+          keke.src.src === "/_next/static/media/forum.192798a1.svg"
+        )
+          sumire.type = "Channel";
+        return sumire;
+      });
+      return tomari;
+    });
 
-  if(chika.length > 0) await refreshLeaderboardData(page + 1)
+  if (chika.length > 0) await refreshLeaderboardData(page + 1);
 };
 
 const app = express();
@@ -116,7 +140,8 @@ app.get("/info", (req, res) => {
 });
 
 app.get("/leaderboard", function (req, res) {
-  let filteredData = levelingData.levels || [];
+  let levelingData = leaderboardData;
+  let filteredData = levelingData.users || [];
 
   const pageCount = Math.ceil(filteredData.length / 10);
   let page = parseInt(req.query.p) || 1;
@@ -134,12 +159,14 @@ app.get("/leaderboard", function (req, res) {
   }
 
   const temp = { ...levelingData }; // Use the spread operator to create a shallow copy
-  temp.levels = filteredData.slice((page - 1) * 10, page * 10);
+  temp.users = filteredData.slice((page - 1) * 10, page * 10);
   let levels = [];
-  for (let i = 0; i < temp.levels.length; i++) {
-    const originalIndex = levelingData.levels.indexOf(temp.levels[i]);
+  for (let i = 0; i < temp.users.length; i++) {
+    const originalIndex = levelingData.users.indexOf(temp.users[i]);
     levels.push(originalIndex + 1);
   }
+  console.log(temp.multipliers);
+  console.log(temp.roles);
   res.render("leaderboard", {
     title: "Leaderboard",
     levels: levels,
@@ -156,6 +183,6 @@ const port = 8080;
 
 app.listen(port, async () => {
   console.log(`App is running on port ${port}`);
-  await refreshLeaderboardData()
-  setInterval(refreshLeaderboardData, 600000)
+  await refreshLeaderboardData();
+  setInterval(refreshLeaderboardData, 600000);
 });
